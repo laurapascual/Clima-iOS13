a57fd792305d8e02c60f29a014a91d6e8fae24fa
Laura Pascual <laurapascualmateo@gmail.com>
2023-09-06 14:40:11 +0200

    Change photo in dark mode and add IB

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -14,11 +14,14 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     
+    @IBOutlet weak var searchTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+    }
+    
 
 }
 
