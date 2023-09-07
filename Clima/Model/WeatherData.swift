6fdd7491e906e3d4a475e1ba3a78940c2e951b14
Laura Pascual <laurapascualmateo@gmail.com>
2023-09-07 10:01:05 +0200

    Add JSON Decoding

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Pascual Mateo Laura on 7/9/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+}
